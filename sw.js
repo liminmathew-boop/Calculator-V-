6009c0cc6f0125087af31c010942fb4f561ce246
liminmathew-boop <liminmathew@gmail.com>
2026-09-21 20:46:42 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,29 @@
-const CACHE_NAME = 'cynosure-calc-v5';
+const CACHE_NAME = 'cynosure-calc-v10';
 const ASSETS = [
   './',
-  './index.html',
-  './icon.png'
+  'index.html',
+  'icon.png',
+  'manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
